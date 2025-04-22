aaac0347dd429d6f5c43afb84ed50a61773ecd0f
Khandan28 <taowqurdu13@gmail.com>
2025-04-22 23:12:02 +0330

    Attributes for Travel + Ticket have been extended.

--- backend/bus/models.py
+++ backend/bus/models.py
@@ -42,7 +42,8 @@ class Terminal(models.Model):
     terminal_id = models.AutoField(primary_key=True, editable=False)
     name = models.CharField(max_length=50)
     city = models.CharField(max_length=25)
-    address = models.TextField()
+
+    address = models.TextField(max_length=250, blank=True, null=True)
     phone = models.CharField(
         max_length=11,
         validators=[phone_number_validator],
@@ -64,17 +65,36 @@ class Bus(models.Model):
 
 
 class Travel(models.Model):
+    travel_id = models.AutoField(primary_key=True, editable=False)
+
     bus = models.ForeignKey(Bus, on_delete=models.CASCADE, related_name="travels")
     terminal = models.ForeignKey(Terminal, on_delete=models.CASCADE, related_name="travels")
     cooperative = models.ForeignKey(Cooperative, on_delete=models.CASCADE, related_name="travels")
 
+    origin = models.CharField(max_length=25, blank=True, null=True)
+    dest = models.CharField(max_length=25, blank=True, null=True)
+    date_time = models.DateTimeField(blank=True, null=True)
+    price = models.PositiveIntegerField(blank=True, null=True)
+    description = models.TextField(max_length=250, blank=True, null=True)
+    capacity = models.IntegerField(blank=True, null=True)
+    
+    # nullable!
+    seat_stat = models.BooleanField(blank=True, null=True)
+
     def __str__(self):
-        pass
+        return f"Travel ID: {self.travel_id}, Origin: {self.origin}, Destination: {self.dest}"
+
 
 class Ticket(models.Model):
-    pk = models.CompositePrimaryKey("user_id", "travel_id")
+    ticket_id = models.AutoField(primary_key=True, editable=False)
+    
     user = models.ForeignKey(UserProfile, on_delete=models.CASCADE, related_name="tickets")
     travel = models.ForeignKey(Travel, on_delete=models.CASCADE, related_name="tickets")
 
+    serial = models.IntegerField(blank=True, null=True)
+    ssn = models.CharField(max_length=10, blank=True, null=True)
+    seat_no = models.PositiveIntegerField(blank=True, null=True)
+    gender = models.BooleanField(blank=True, null=True)
+
     def __str__(self):
-        pass
+        return f"Ticket ID: {self.ticket_id}, User: {self.user.username}, Travel ID: {self.travel.travel_id}"
