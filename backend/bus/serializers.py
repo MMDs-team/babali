c70fabb51e828c75f6e7ff7c37efbe4dfd431606
mmd <mohmad1382m@gmail.com>
2025-04-28 21:32:55 +0330

    Add bus API endpoints and travel serializer for available travels

---
+++ backend/bus/serializers.py
@@ -0,0 +1,19 @@
+from rest_framework import serializers
+from bus.models import *
+
+class TravelSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Travel
+        fields = [
+            'travel_id',
+            'bus',
+            'terminal',
+            'cooperative',
+            'origin',
+            'dest',
+            'date_time',
+            'price',
+            'description',
+            'capacity',
+        ]
