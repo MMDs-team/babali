cb8d745d81d11bccaccf6ab2e5e362fa12fc5996
mmd <mohmad1382m@gmail.com>
2025-08-08 00:02:20 +0330

    Add TravelFilter for filtering travel data by origin, destination, and date

--- backend/bus/views/travel_views.py
+++ backend/bus/views/travel_views.py
@@ -6,19 +6,9 @@ from rest_framework import viewsets, status
 
 from bus.serializers.travel_serializers import TravelSerializer
 from bus.models import Travel
+from bus.filters import TravelFilter
 
 from django_filters.rest_framework import DjangoFilterBackend
-from django_filters import rest_framework as filters
-
-
-class TravelFilter(filters.FilterSet):
-    origin = filters.CharFilter(field_name='origin', lookup_expr='exact')
-    destination = filters.CharFilter(field_name='dest', lookup_expr='exact')
-    date = filters.DateFilter(field_name='date_time', lookup_expr='date')
-
-    class Meta:
-        model = Travel
-        fields = ['origin', 'destination', 'date']
 
 
 class TravelViewSet(ListModelMixin, RetrieveModelMixin, GenericViewSet):
@@ -27,26 +17,3 @@ class TravelViewSet(ListModelMixin, RetrieveModelMixin, GenericViewSet):
 
     filter_backends = [DjangoFilterBackend]
     filterset_class = TravelFilter
-
-    @action(detail=False, methods=['get'])
-    def available(self, request):
-        
-        origin_city = request.GET.get("origin")
-        dest_city = request.GET.get("destination")
-        date = request.GET.get("date")
-
-        try:
-            travels = Travel.objects.filter(
-                origin=origin_city,
-                dest=dest_city,
-                date_time__date=date
-            ).order_by('date_time')
-
-            serializer = TravelSerializer(travels, many=True)
-            return Response(serializer.data)
-
-        except Exception as error:
-            return Response(
-                {'error': str(error)},
-                status=status.HTTP_500_INTERNAL_SERVER_ERROR
-            )
