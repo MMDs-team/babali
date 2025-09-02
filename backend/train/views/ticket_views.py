0279b75c28346064a52b8893bc500cbc94df1131
Khandan <mkhandani13@gmail.com>
2025-09-02 14:21:37 +0330

    Add cancel order logic to train sub-system.

--- backend/train/views/ticket_views.py
+++ backend/train/views/ticket_views.py
@@ -74,6 +74,7 @@ class TicketViewSet(ListModelMixin, RetrieveModelMixin, GenericViewSet):
                     Ticket(
                         **item_data,
                         seat_no = next_seat_number,
+                        get_full_compartment=get_full_com,
                         compartment_no = compartment_num,
                         serial=curr_serial_no,
                         payment_due_datetime=payment_due_datetime
@@ -110,26 +111,26 @@ class TicketViewSet(ListModelMixin, RetrieveModelMixin, GenericViewSet):
         return Response({'error': 'You have to supply both serial & status query params.'}, status=status.HTTP_406_NOT_ACCEPTABLE)
 
 
-    # TODO: Test this endpoint then integrate.
-    # @action(detail=False, methods=['patch'])
-    # def cancel(self, request):
-    #     serial = request.data['serial']
-    #     tickets = Ticket.objects.filter(serial=serial, status='A', canceled=False)
-    #     if len(tickets):
-    #         travel = tickets[0].travel
-    #         for ticket in tickets:
-    #             seat_no = str(ticket.seat_no)
+    @action(detail=False, methods=['patch'])
+    def cancel(self, request):
+        serial = request.data['serial']
+        tickets = Ticket.objects.filter(serial=serial, status='A', canceled=False)
+        if len(tickets):
+            travel = tickets[0].travel
+            for ticket in tickets:
+                seat_no = str(ticket.seat_no)
 
-    #             # TODO: handle the issue with phone part.
-    #             # del travel.seat_stat[seat_no]['phone']
-    #             travel.seat_stat[seat_no]['gender'] = 'E'
+                del travel.seat_stat[seat_no]['user_phone']
+                travel.seat_stat[seat_no]['gender'] = 'E'
 
-    #         tickets.update(canceled=True)
-    #         # TODO: Logic for payment rollback.
+            travel.capacity += travel.train.compartment_capacity if tickets[0].get_full_compartment else len(tickets)
+            travel.save()
+            tickets.update(canceled=True)
+            # TODO: Logic for payment rollback.
 
-    #         return Response({'msg': f'Tickets with serial={serial} have been canceled successfully.'})
+            return Response({'msg': f'Tickets with serial={serial} have been canceled successfully.'})
 
-    #     return Response({'error': f'There is no ticket with serial={serial} to cancel.'})
+        return Response({'error': f'There is no ticket with serial={serial} to cancel.'})
     
 
     @action(detail=False, methods=['post'])
