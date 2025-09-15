d34afc7f9b0c12fc9183359c30dcf7945e97c270
mohammad-ghaderi <mohammadunique2002@gmail.com>
2025-09-15 19:18:57 +0330

    Revert "adding default host and port incase missing .env.local file"
    This reverts commit 50e37a89075a2b4621e8e24c58f3b9f8f69e1343.

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -12,8 +12,6 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 
 import os
 from pathlib import Path
-from dotenv import load_dotenv
-
 
 # Use custome user
 AUTH_USER_MODEL = 'babali.User'
@@ -112,16 +110,19 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-load_dotenv()
-
 DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # }
+
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': os.getenv('DATABASE_NAME'),
-        'USER': os.getenv('DATABASE_USER'),
-        'PASSWORD': os.getenv('DATABASE_PASSWORD'),
-        'HOST': os.getenv('DATABASE_HOST'),  
-        'PORT': os.getenv('DATABASE_PORT'),   
+        'ENGINE': 'django.db.backends.mysql', 
+        'NAME': os.environ.get('DATABASE_NAME'),
+        'USER': os.environ.get('DATABASE_USER'),
+        'PASSWORD': os.environ.get('DATABASE_PASSWORD'),
+        'HOST': os.environ.get('DATABASE_HOST'),
+        'PORT': os.environ.get('DATABASE_PORT'),
     }
 }
 
